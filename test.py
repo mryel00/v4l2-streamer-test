bbe2816a69eaaa1cf09ff619342d534907f1edd1
Patrick Gehrsitz <mryel00.github@gmail.com>
2024-10-10 16:16:45 +0200

    fix: fix testing file
    Signed-off-by: Patrick Gehrsitz <mryel00.github@gmail.com>

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
+# https://www.marcusfolkesson.se/blog/capture-a-picture-with-v4l2/
+
 from v4l2 import ctl, constants
 
 cap = ctl.get_camera_capabilities('/dev/video0')
 print(cap)
 
 if not ((cap['capabilities'] & constants.V4L2_CAP_VIDEO_CAPTURE) and (cap['capabilities'] & constants.V4L2_CAP_STREAMING)):
+    pass
+
